3e6032c8d3ae6160f731a9b094b60386d7267514
Fidelya1988 <oleksandram1988@gmail.com>
2021-04-23 03:00:14 +0300

    сделала рамки у заголовков, поменяла скрипты, чтоб при клике на новости, удалалось фоновое изображение, поменяла иконку фб, добавила контакты в main

--- script/nav.js
+++ script/nav.js
@@ -9,13 +9,24 @@ const contentContainer = document.querySelector('#main-content');
 const contentElements = document.querySelectorAll('.content-item');
 const newsElement = contentContainer.querySelector('#news');
 const bandElement = contentContainer.querySelector('#band');
-
-const showElement = (element) => {
+const contactsElement = contentContainer.querySelector('#contacts');
+const backgroundImage = document.querySelector('#background-img')
+contentContainer.parentElement.style.display = 'none';
+const showElement = (element, hideImg) => {
+    contentContainer.parentElement.style.display = 'block';
     contentContainer.style.display = 'block';
+   
     element.style.display = 'block';
     for (let contentElement of contentElements) {
         if (contentElement.id != element.id) {
             contentElement.style.display = 'none';
+        }
+
+        if (hideImg) {
+
+            backgroundImage.style.display = "none"
+        } if (!hideImg) {
+            backgroundImage.style.display = "block"
         }
     }
 }
@@ -26,22 +37,28 @@ for (let button of buttons) {
 
         switch (button.id) {
             case newsButton:
-              return  showElement(newsElement);
+                return showElement(newsElement, true);
+
             case bandButton:
-              return  showElement(bandElement);
+                return showElement(bandElement);
+            case contactsButton:
+                return showElement(contactsElement);
             case mainButton:
                 for (let contentElement of contentElements) {
+                    contentContainer.parentElement.style.display = 'none';
                     contentContainer.style.display = 'none';
                     contentElement.style.display = 'none';
+                    backgroundImage.style.display = 'block';
                 }
+
         }
 
 
 
-    
- 
-}
-        
+
+
+    }
+
     )
 
 }
